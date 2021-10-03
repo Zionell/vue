214a9ab173b172a1939db46edf57d0d7ca3659b6
Zionell <adilhan.askarov@gmail.com>
2021-10-03 13:23:28 +0400

    hw1

--- hw/src/components/Calculator.vue
+++ hw/src/components/Calculator.vue
@@ -1,61 +1,17 @@
 <template>
-  <div class="wrapper">
+  <div>
     <div>
-      <input
-          v-model.number="operand1"
-      />
-      <input
-          v-model.number="operand2"
-      />
-      = {{ result }}
+      <input v-model="operand1"/>
+      <input v-model="operand2"/>
+      = {{result}}
     </div>
-    <label>
-      <input
-          type="checkbox"
-          v-model="check"
-      >
-      Отобразить экранную клавиатуру
-    </label>
-    <label v-if="check">
-      <input
-          type="radio"
-          name="changeOperand"
-          value="operand1"
-          v-model="operand"
-      >
-      Operand1
-    </label>
-    <label v-if="check">
-      <input
-          type="radio"
-          name="changeOperand"
-          value="operand2"
-          v-model="operand"
-      >
-      Operand2
-    </label>
     <div class="keyboard">
-      <div v-show="check" class="keyboard_numbers">
-        <button
-            v-for="(num,ind) in numbers"
-            :key="ind"
-            @click="writeNum(ind)"
-        >
-          {{ ind }}
-        </button>
-        <button
-            @click="deleteNum()"
-        >
-          &#129044;
-        </button>
-      </div>
-      <button
-          v-for="operator in operators"
-          :key="operator"
-          @click="calcHandler(operator)"
-      >
-        {{ operator }}
-      </button>
+      <button @click="plusCalc">+</button>
+      <button @click="minusCalc">-</button>
+      <button @click="multiplCalc">*</button>
+      <button @click="devisionCalc">/</button>
+      <button @click="expCalc">**</button>
+      <button @click="ceilCalc">//</button>
     </div>
   </div>
 </template>
@@ -63,93 +19,36 @@
 <script>
 export default {
   name: "Calculator",
-  data() {
+  data(){
     return {
-      operand1Arr:[],
-      operand2Arr:[],
-      operand: 1,
-      check:false,
-      result: 0,
-      numbers: Array(10),
-      operators: [
-        'plus',
-        'minus',
-        'multiply',
-        'devide',
-        'degree',
-        'floorDevide'
-      ]
-    }
-  },
-  computed:{
-    operand1(){
-      return this.operand1Arr.join('')
-    },
-    operand2(){
-      return this.operand2Arr.join('')
+      operand1: 0,
+      operand2: 0,
+      result: 0
     }
   },
   methods: {
-    writeNum(num) {
-      if(this.operand=="operand1"){
-        return this.operand1Arr.push(num)
-      }else{
-        return this.operand2Arr.push(num)
-      }
+    plusCalc(){
+      return this.result = parseInt(this.operand1) + parseInt(this.operand2);
     },
-    deleteNum() {
-      if(this.operand=="operand1"){
-        return this.operand1Arr.pop()
-      }else{
-        return this.operand2Arr.pop()
-      }
+    minusCalc(){
+      return this.result = this.operand1-this.operand2;
     },
-    calcHandler(action) {
-      switch (action) {
-        case 'plus':
-          return this.result = parseInt(this.operand1) + parseInt(this.operand2);
-        case 'minus':
-          return this.result = this.operand1 - this.operand2;
-        case 'multiply':
-          return this.result = this.operand1 * this.operand2;
-        case 'devide':
-          if (this.operand2 !== 0) {
-            return this.result = this.operand1 / this.operand2;
-          }
-          alert("На ноль делить нельзя")
-          break;
-        case 'degree':
-          return this.result = this.operand1 ** this.operand2;
-        case 'floorDevide':
-          if (this.operand2 !== 0) {
-            return this.result = Math.floor(this.operand1 / this.operand2);
-          }
-          alert("На ноль делить нельзя")
-          break;
-      }
+    multiplCalc(){
+      return this.result = this.operand1*this.operand2;
+    },
+    devisionCalc(){
+      return this.result = this.operand1/this.operand2;
+    },
+    expCalc(){
+      return this.result = this.operand1**this.operand2;
+    },
+    ceilCalc(){
+      return this.result = Math.floor(this.operand1/this.operand2);
     }
   }
 }
 </script>
 
 <style scoped>
-.wrapper {
-  display: flex;
-  flex-direction: column;
-  align-content: center;
-}
 
-.keyboard {
-  display: grid;
-  grid-template-columns: 10fr 2fr;
-  grid-template-columns: 0fr 0fr;
-  justify-content: center;
-}
-
-.keyboard_numbers {
-  grid-row: 1/8;
-  display: grid;
-  grid-template-columns: repeat(3, 40px);
-  grid-template-rows: repeat(4, 40px);
-}
 </style>
